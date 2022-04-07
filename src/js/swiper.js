666699d923f40776866c6c9bb692eb351c167c9b
timikos <timikos@mail.ru>
2022-04-07 17:11:29 +0300

    add slider

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,15 +1,34 @@
 import Swiper from 'swiper';
 
+const slider = document.querySelector(".elements__menu");
 
-const swiper = new Swiper('.swiper', {
-  direction: 'horizontal',
-  loop: true,
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-  pagination: {
-    el: '.swiper-pagination',
-  },
+let mySwiper;
+
+function mobileSlider() {
+  if (window.innerWidth < 768 && slider.dataset.mobile == "false"){
+    mySwiper = new Swiper(slider, {
+      slidesPerView:1,
+      loop: true,
+      slideClass: "swiper-slide",
+      pagination: {
+        el: ".swiper-pagination",
+        type: "bullets",
+      },
+    });
+
+    slider.dataset.mobile = "true";
+  }
+  if (window.innerWidth >= 768) {
+    slider.dataset.mobile = "false";
+
+    if (slider.classList.contains("swiper-initialized")) {
+      mySwiper.destroy();
+    }
+  }
+};
+
+mobileSlider();
+
+window.addEventListener("resize", () => {
+  mobileSlider();
 });
-
